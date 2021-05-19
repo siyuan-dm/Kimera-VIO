994901c1b3dc5244877bf9e64ba4276b1cbcbb22
Siyuan Liu <siyuan.liu@deepmirror.com>
2021-05-19 17:59:43 +0800

    docker change

--- scripts/docker/kimera_vio_docker.bash
+++ scripts/docker/kimera_vio_docker.bash
@@ -3,9 +3,14 @@
 # Allow X server connection
 xhost +local:root
 docker run -it --rm \
+    --gpus all \
+    -e DISPLAY \
+    -e NVIDIA_DRIVER_CAPABILITY=all \
+    -e NVIDIA_VISIBLE_DEVICES=all \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --privileged \
     kimera_vio
 # Disallow X server connection
 xhost -local:root
